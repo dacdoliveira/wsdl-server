629a33de1d6836846a5ead4050e59c4874be402d
Diers <Diers@LAPTOP-VTCD2RNQ>
2021-08-10 15:01:00 -0300

    comita versao inicial do projeto com spring boot

---
+++ src/test/java/com/dacdoliveira/wsdlserver/WsdlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dacdoliveira.wsdlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
